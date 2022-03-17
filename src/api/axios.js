e3ab834617b33eebd49c9371af80859a2ff81cdb
stoneboat <tood2111@gmail.com>
2022-03-17 22:07:25 +0900

    key 변경

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-
+const API_KEY = process.env.REACT_APP_NETFLIX_API_KEY;
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "33252256be1722b98d7c20fbcc7fab5d",
+    api_key: `${API_KEY}`,
     language: "ko-KR",
   },
 });
