e4a6a75561a7661a759806fa8610a858a19846b8
stoneboat <tood2111@gmail.com>
2022-03-17 23:45:56 +0900

    test

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@ const API_KEY = process.env.REACT_APP_NETFLIX_API_KEY;
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: `${API_KEY}`,
+    api_key: `33252256be1722b98d7c20fbcc7fab5d`,
     language: "ko-KR",
   },
 });
